4d1fe2318ce18ce63e6632e6aeda74fa035fe7f9
Soar0202 <shohei22cosmos@gmail.com>
2020-02-15 08:44:40 +0900

    first commit

---
+++ COVID-19/OverAll/OverAllTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OverAllTableViewCell.swift
+//  COVID-19
+//
+//  Created by 西田翔平 on 2020/02/14.
+//  Copyright © 2020 西田翔平. All rights reserved.
+//
+
+import UIKit
+
+class OverAllTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
